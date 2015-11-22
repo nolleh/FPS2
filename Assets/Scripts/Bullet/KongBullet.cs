960e15e19de2f595a30d6e1e924178d681b3b1e5
nolleh <nolleh7707@gmail.com>
2015-11-22 17:20:39 +0900

    first

---
+++ Assets/Scripts/Bullet/KongBullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//namespace TEST {
+public class KongBullet : MonoBehaviour {
+
+	private Vector3 vDir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	public void init(Vector3 _vDir) {
+		vDir = _vDir;
+	}
+
+	void Move() {
+		Vector3 vPos = transform.localPosition;
+		vPos.z += vDir.z;
+		transform.localPosition = vPos;
+	}
+}
+//}
